ef2cc7db44b0037803bc53e03d1bcb44270f2265
Rasmus Möllmark <rasmusmollmark@gmail.com>
2023-10-17 14:15:56 +0200

    Ny

--- Nim_Game/Program.cs
+++ Nim_Game/Program.cs
@@ -1,31 +1,41 @@
 ﻿class program
 {
+    /// <summary>
+    /// This property always returns a value &lt; 1.
+    /// </summary>
+    ///<returns>description</returns>
+    ///<param name="name">description</param>
     public static void Main(string[] args)
     {
         greetUser();
         while (true)
         {
-            int[] noOfSticksEachRow = { 5, 5, 5 };
-            int userChoice = menu();
-            if (userChoice == 0)
+            int[] noOfSticksEachRow = { 5, 5, 5 };  //Skapar array för stickorna. 3 rader, 5 stickor i varje.
+            int userChoice = menu(); //Skriver ut meny samt kollar om användaren vill avsluta, spela mot någon eller mot datorn.
+            if (userChoice == 0) //Väljer användaren 0 så avslutas programmet.
             {
                 Environment.Exit(0);
             }
 
-            switch (userChoice)
+            switch (userChoice) //Kollar vad användaren valt
             {
                 case 1:
-                    playWithComputer(noOfSticksEachRow);
+                    playWithComputer(noOfSticksEachRow); //Spela med datorn
                     break;
                 case 2:
-                    playTogether(noOfSticksEachRow);
+                    playTogether(noOfSticksEachRow); //Spela mot varandra
                     break;
                 default:
+                    Console.WriteLine("\nMåste vara en siffra 0, 1 eller 2.\n"); //Om användaren skrivit in en int som inte är 0-2. 
                     break;
             }
         }
     }
 
+    /// <summary>
+    /// Välkomnar användare.
+    /// </summary>
+    
     private static void greetUser()
     {
         Console.WriteLine("Hej och välkommen till nimspelet!\n" +
@@ -34,46 +44,51 @@
             " för att till slut plocka den sista stickan.\n" +
             "Man får ta hur många stickor ur EN hög under sin tur.\n" +
             "Man vinnar som sagt genom att ta den sista stickan.\n" +
-            "För att plocka stickor skriver du in \"<rad> <antal stickor>\".\nEx \"1 2\" för att plocka 2 stickor från rad 1. \n");
+            "För att plocka stickor skriver du in \"<rad> <antal stickor>\".\nEx \"1 2\" för att plocka 2 stickor från rad 1. \n"); //Välkomnande
     }
 
+    /// <summary>
+    /// Metoden som sköter flerspelarläget. Här görs de större metodanropen.
+    /// </summary>
+    ///<param name="noOfSticksEachRow">En array som innehåller antalet stickor per rad.</param>
     private static void playTogether(int[] noOfSticksEachRow)
     {
-        string whichPlayerIsPlaying = "Spelare 1:s";
-        bool gameOver = false;
-        while (!gameOver)
+        string[] playersName = getPlayersName().Split(); //Hämtar spelarnas namn och lagrar i en array, spelare 1 på plats 0, spelare 2 på plats 1.
+        string whichPlayerIsPlaying = playersName[0]; //Sätter spelare 1 som startspelare
+        bool gameOver = false; //Initierar en bool som ska se om spelet är klart eller om det ska fortsätta
+        while (!gameOver) //Kollar om spelet är klart
         {
-            createGame(noOfSticksEachRow);
+            printSticks(noOfSticksEachRow); //Skriver ut "spelplanen"
             
             try
             {
-                string[] userChoice = getUserChoice(whichPlayerIsPlaying);
-                int tooBigOrSmallNumber = noOfSticksEachRow[int.Parse(userChoice[0]) - 1] - int.Parse(userChoice[1]);
-                if (tooBigOrSmallNumber > 5 || tooBigOrSmallNumber < 0)
+                string[] userChoice = getUserChoice(whichPlayerIsPlaying); //Hämtar användarens input
+                if (isInputTooBigOrSmall(userChoice, noOfSticksEachRow)) //Kollar om input är för stor eller liten
                 {
                     Console.WriteLine("Du har tagit för många pinnar eller stoppat in för många pinnar!");
-                    continue;
+                    continue; //Hoppar högst upp i while-loopen
                 }
                 else
                 {
-                    noOfSticksEachRow[(int.Parse(userChoice[0]))-1] -= int.Parse(userChoice[1]);
-                    if (noOfSticksEachRow[0] == 0 && noOfSticksEachRow[1] == 0 && noOfSticksEachRow[2] == 0)
+                    noOfSticksEachRow[(int.Parse(userChoice[0]))-1] -= int.Parse(userChoice[1]); //Tar bort stickorna 
+                    if (noOfSticksEachRow[0] == 0 && noOfSticksEachRow[1] == 0 && noOfSticksEachRow[2] == 0) //Kollar om alla stickor plockats
                     {
-                        gameOver = true;
-                        Console.WriteLine("\n"+whichPlayerIsPlaying + " vann spelet!\n\n");
+                        gameOver = true; //Spelet är slut
+                        Console.WriteLine("\n"+whichPlayerIsPlaying + " vann spelet!\n\n"); //Skriver ut vinnaren
+                        askIfGameShouldContinue(); //Fråga användaren om denna vill spela igen eller avsluta programmet
                     }
-                    if (whichPlayerIsPlaying.Equals("Spelare 1:s"))
+                    if (whichPlayerIsPlaying.Equals(playersName[0])) //Kollar vems tur det varit och sätter nästa tur till den andra spelaren
                     {
-                        whichPlayerIsPlaying = "Spelare 2:s";
+                        whichPlayerIsPlaying = playersName[1];
                     }
                     else
                     {
-                        whichPlayerIsPlaying = "Spelare 1:s";
+                        whichPlayerIsPlaying = playersName[0];
                     }
                 }
                
             }
-            catch
+            catch 
             {
                 Console.WriteLine("Felinmat, endast siffror");
                 continue;
@@ -81,11 +96,58 @@
         }
     }
 
-    
+    /// <summary>
+    /// Frågar användaren om spelet ska fortsätta.
+    /// </summary>
+    private static void askIfGameShouldContinue()
+    {
+        while (true)
+        {
+            Console.Write("J för spela igen\nN för avsluta\nDitt val: ");
+            string input = Console.ReadLine();
+            if(input.Equals("j") || input.Equals("J")){
+                return;
+            }
+            else if(input.Equals("n") || input.Equals("N"))
+            {
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine("Fel inmat!\n");
+            }
+        }
+    }
 
+    /// <summary>
+    /// Metoden som sköter flerspelarläget. Här görs de större metodanropen.
+    /// </summary>
+    /// <returns> En sträng med båda spelares namn. "Spelare 1" + " Spelare 2" </returns>
+    private static string getPlayersName()
+    {
+        Console.Write("Skriv in spelare 1:s namn: ");
+        string names = Console.ReadLine().Trim();
+        Console.Write("Skriv in spelare 2:s namn: ");
+        names += " "+Console.ReadLine().Trim();
+        return names;
+    }
+
+    /// <summary>
+    /// Hämtar användarens val för vilken rad de plockar från samt hur många stickor.
+    /// </summary>
+    /// <param name="whichPlayerIsPlaying">En sträng innehållande en av spelarnas namn.</param>
+    /// <returns> En array med användarens val: rad på plats 0 och antal på plats 1. </returns>
     private static string[] getUserChoice(string whichPlayerIsPlaying)
     {
-        Console.WriteLine(whichPlayerIsPlaying + " tur");
+        if (whichPlayerIsPlaying[whichPlayerIsPlaying.Length - 1].Equals('s'))
+        {
+            Console.WriteLine(whichPlayerIsPlaying + " tur");
+        }
+        else
+        {
+            Console.WriteLine(whichPlayerIsPlaying + "s tur");
+        }
+        
         Console.Write("Skriv in vilken rad och hur många stickor du vill plocka enligt \"<rad> <antal>\": ");
         string[] input = Console.ReadLine().Split();
         if (int.Parse(input[0]) < 0 && int.Parse(input[0]) > 3) 
@@ -96,11 +158,10 @@
         
     }
 
-    private static void createGame(int[] noOfSticksEachRow)
-    {
-        printSticks(noOfSticksEachRow);
-    }
-
+    /// <summary>
+    /// Skriver ut "spelplanen".
+    /// </summary>
+    ///<param name="noOfSticksEachRow">En array som innehåller antalet stickor per rad.</param>
     private static void printSticks(int[] noOfSticksEachRow)
     {
         Console.WriteLine("\t*****");
@@ -116,6 +177,10 @@
         Console.WriteLine("\t*****");
     }
 
+    /// <summary>
+    /// Metoden som sköter datorläget. Här görs de större metodanropen.
+    /// </summary>
+    ///<param name="noOfSticksEachRow">En array som innehåller antalet stickor per rad.</param>
     private static void playWithComputer(int[] noOfSticksEachRow)
     {
         bool gameOver = false;
@@ -127,14 +192,15 @@
         }
         while (!gameOver)
         {
-            createGame(noOfSticksEachRow);
+            printSticks(noOfSticksEachRow);
             try
             {
                 if (playersTurn)
                 {
                     string[] userChoice = getUserChoice("Spelares");
-                    int tooBigOrSmallNumber = noOfSticksEachRow[int.Parse(userChoice[0]) - 1] - int.Parse(userChoice[1]);
-                    if (tooBigOrSmallNumber > 5 || tooBigOrSmallNumber < 0)
+                   
+                    
+                    if (isInputTooBigOrSmall(userChoice, noOfSticksEachRow))
                     {
                         Console.WriteLine("Du har tagit för många pinnar eller stoppat in för många pinnar!");
                         continue;
@@ -159,18 +225,38 @@
         }
     }
 
+    /// <summary>
+    /// Kollar om input som skickats är korrekt för hur många stickor som finns kvar.
+    /// </summary>
+    ///<param name="noOfSticksEachRow">En array som innehåller antalet stickor per rad.</param>
+    /// <param name="userChoice"> En array som innehåller vilken rad och hur många stickor som användaren valt att plocka.</param>
+    /// <returns> True om inputen är för för stor/liten och false om inputen är korrekt.</returns>
+    private static bool isInputTooBigOrSmall(string[] userChoice, int[] noOfSticksEachRow)
+    {
+        int tooBigOrSmallNumber = noOfSticksEachRow[int.Parse(userChoice[0]) - 1] - int.Parse(userChoice[1]);
+        if( tooBigOrSmallNumber < 0 || tooBigOrSmallNumber > 5) 
+        {
+            return true;
+        }
+        return false;
+    }
+
     private static void generateComputerMove(int[] noOfSticksEachRow)
     {
         throw new NotImplementedException();
     }
 
+    /// <summary>
+    /// Skriver ut meny.
+    /// </summary>
+    /// <returns>Användarens val i menyn.</returns>
     private static int menu()
     {
         while (true)
         {
             try
             {
-                Console.Write("Välj om du vill spela med:\n" +
+                Console.Write("\nVälj om du vill spela med:\n" +
                     "1. Datorn\n" +
                     "2. Två spelare\n" +
                     "0. För att avsluta\n" +
@@ -179,7 +265,7 @@
             }
             catch
             {
-                Console.WriteLine("Felinmat, välj mellan 1 och 2.");
+                Console.WriteLine("Felinmat, välj mellan 0, 1 och 2.");
                 continue;
 
             }
